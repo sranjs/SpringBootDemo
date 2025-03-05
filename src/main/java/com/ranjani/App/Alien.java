8974923d9d2b3453c5d98bca973769d6715cf775
sranjs <ranjanisr945@gmail.com>
2025-03-05 14:57:42 +0530

    MY DI SPRINGBOOT APPLICATION

---
+++ src/main/java/com/ranjani/App/Alien.java
@@ -0,0 +1,9 @@
+package com.ranjani.App;
+
+import org.springframework.stereotype.Component;
+@Component
+public class Alien {
+    public void code(){
+        System.out.println("Coding");
+    }
+}
